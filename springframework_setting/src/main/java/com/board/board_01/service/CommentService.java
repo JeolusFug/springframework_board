c9498695c52ece0f2fba447cd6758281b09b0868
JeolusFug <155438195+JeolusFug@users.noreply.github.com>
2024-03-08 17:47:35 +0900

    Add files via upload

--- springframework_setting/src/main/java/com/board/board_01/service/CommentService.java
+++ springframework_setting/src/main/java/com/board/board_01/service/CommentService.java
@@ -4,10 +4,11 @@ package com.board.board_01.service;
 import com.board.board_01.dto.CommentDTO;
 import com.board.board_01.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +24,23 @@ public class CommentService {
     public List<CommentDTO> findAll(Long boardId) {
         return commentRepository.findAll(boardId);
     }
+    /* public void commentdelete(Long id) {
+        commentRepository.commentdelete(id);
+    } */
+
     public void commentdelete(Long id) {
+        // System.out.println("comment-delete-btn = " + id);
         commentRepository.commentdelete(id);
     }
+
+    public String findWriter(Long id) {
+        return commentRepository.findWriter(id);
+    }
+
+    // 댓글 수정
+    // コメントしゅうせい
+    public void commentupdate(Map<String, Object> updateTool) {
+        commentRepository.commentupdate(updateTool);
+    }
+
 }
