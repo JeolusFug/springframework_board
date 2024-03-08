45f0bcbdab456d2473fbb24555c6d9bafb35b6b4
JeolusFug <155438195+JeolusFug@users.noreply.github.com>
2024-03-08 17:43:42 +0900

    Add files via upload

--- springframework_setting/src/main/java/com/board/board_01/controller/CommentController.java
+++ springframework_setting/src/main/java/com/board/board_01/controller/CommentController.java
@@ -4,12 +4,13 @@ package com.board.board_01.controller;
 import com.board.board_01.dto.CommentDTO;
 import com.board.board_01.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,9 +31,64 @@ public class CommentController {
         List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
         return commentDTOList;
     }
-    @PostMapping("/delete")
+    /* @PostMapping("/delete")
     public String commentdelete(@RequestParam("id") Long id) {
         commentService.commentdelete(id);
         return "redirect:/board?id=${board.id}";
+    } */
+
+    @PostMapping("/DeleteCheck")
+    public @ResponseBody String DeleteCheck(HttpSession session, @ModelAttribute CommentDTO commentDTO,
+                                             @RequestParam("commentId") Long id) {
+        String Writer = commentService.findWriter(id);
+        String loginId = (String) session.getAttribute("loginEmail");
+        //System.out.println("comment id = " + id + ", commentWriter = " + Writer + ", loginId = " + loginId);
+        //System.out.println("id = " + id );@RequestParam("commentId") Long id
+        if (loginId.equals(Writer)) {
+            return "Matched";
+        } else {
+            return "NotMatched";
+        }
+    }
+
+    @PostMapping("/Delete")
+    public @ResponseBody List<CommentDTO> Delete(@ModelAttribute CommentDTO commentDTO,
+                                                  @RequestParam(value = "commentId") Long id) {
+        commentService.commentdelete(id);
+        List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
+        //System.out.println("commentDTOList = " + commentDTOList + ",boardId " + commentDTO.getBoardId());
+        return commentDTOList;
+    }
+
+    // 수정 버튼을 눌렀을 시 작성자와 로그인 아이디를 확인하기 위함
+    @PostMapping("/UpdateCheck")
+    public @ResponseBody String updateCheck (HttpSession session, @RequestParam("commentId") Long id) {
+        String Writer = commentService.findWriter(id);
+        String loginId = (String) session.getAttribute("loginEmail");
+        //System.out.println("Writer = " + Writer + ", loginId = " + loginId);
+        if (loginId.equals(Writer)) {
+            return "Same";
+        } else {
+            return "NOPE";
+        }
+    }
+
+    // 수정 기능
+    // しゅうせいきのう
+    @PostMapping("/update")
+    public @ResponseBody List<CommentDTO> update(@ModelAttribute CommentDTO commentDTO,
+                                                 @RequestParam("commentId") Long id,
+                                                 @RequestParam("commentContents") String contents) {
+        Map<String, Object> updateTool = new HashMap<>();
+        updateTool.put("id", id);
+        updateTool.put("contents", contents);
+        // System.out.println("id = " + id + ", contents = " + contents);
+        // 수정 후 전체 댓글 목록을 다시 불러오기
+        // しゅうせいご、ぜんたいのコメントをさいよみこむ
+        commentService.commentupdate(updateTool);
+
+        List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
+        System.out.println("commentDTOList2 = " + commentDTOList);
+        return commentDTOList;
     }
 }
